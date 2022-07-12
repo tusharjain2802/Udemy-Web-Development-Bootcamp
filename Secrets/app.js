faea453e1ce9a01fbb2d4d6828e76ede27387a1c
Tushar Jain <72308720+tusharjain2802@users.noreply.github.com>
2022-07-12 20:50:47 +0530

    Added passport modules to authenticate users

--- Secrets/app.js
+++ Secrets/app.js
@@ -6,27 +6,36 @@ const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrupt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-console.log(process.env.API_KEY);
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+app.use(session({
+    secret: "Our little Secret.",
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+  
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
-
 const userSchema =new mongoose.Schema({
     email: String,
     password: String
 });
-// const secret = "thisisourlittlesecret.";
-// cut it and put in in .env file
 
-var secret = process.env.SECRET;
-
+userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser()); 
 
 app.get("/", function(req,res){
     res.render("home");
@@ -39,38 +48,56 @@ app.get("/login", function(req,res){
 app.get("/register", function(req,res){
     res.render("register")
 });
+app.get("/submit", function(req,res){
+    res.render("submit")
+});
+
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    } else{
+        res.redirect("/login");
+    }
+    // You can directly revisit /secrets page after registering or login but after clearing cookies you can't
+});
+
+app.get("/logout",function(req,res){
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+});
 
 app.post("/register", function(req,res){ 
-    bcrupt.hash(req.body.password, saltRounds, function(err, hash){
-        const newUser = new User({
-            email:req.body.username,
-            password: hash
-        });
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets")
-            }
-        });
+    User.register({username:req.body.username},req.body.password, function(err,user){
+        //the register function of passport will automatically change the password into md5 and salt and save it into mongoDB db.
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets");
+                // Earlier we never let out user users visit the secret route. we just used to render the secrets page on current route. But now we have authenticated user who has registered.
+            });
+        }
     });
 });
 
 app.post("/login", function(req,res){
-    const username = req.body.username;
-    const password= req.body.password;
-    User.findOne({email:username}, function(err, foundUser){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
         if(err){
             console.log(err);
         }
         else{
-            if(foundUser){
-                    bcrupt.compare(password, foundUser.password,function(err,result){
-                        if(result == true){ // it means password matched
-                            res.render("secrets");
-                        }
-                    });
-            }
+            passport.authenticate("local")(req,res, function(){
+                //this will send a cookie and authenticate the user.
+                res.redirect("/secrets");
+                
+            });
         }
     });
 });
